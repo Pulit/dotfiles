1b3976e3f83aaa6d187ff73cb1ab8c478d776368
myaox <llgoodrr@gmail.com>
2017-08-19 09:07:51 +0900

    .vimrc

--- install.sh
+++ install.sh
@@ -1,38 +1,18 @@
-#!/bin/bash
+TPATH=$HOME/dotfiles
 
-set -u
+echo "\$DOTPATH : $DOTPATH"
+echo "Created dotfile symbolic links."
+for f in .??*
+do
+ # Exclude files
+ if [ "$f" = ".git" ] || [ "$f" = ".gitignore" ];then
+	continue
+ fi
 
-# 実行場所のディレクトリを取得
-THIS_DIR=$(cd $(dirname $0); pwd)
-
-cd $THIS_DIR
-git submodule init
-git submodule update
-
-echo "start setup..."
-for f in .??*; do
-  ["$f"=".git"] && continue
-  ["$f"=".gitconfig.local.template"] && continue
-  ["$f"=".require_oh-my-zsh"] && continue
-  ["$f"=".gitmodules"] && continue
-
-  ln -snfv ~/dotfiles/"$f" ~/
+ ln -sf "$DOTPATH"/"$f" "$HOME"/"$f"
+ if [ $? -eq 0 ]; then
+	 printf "    %-25s -> %s\n" "\$DOTPATH/$f" "\$HOME/$f"
+ fi
 done
+echo ""
 
-[ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
-
-# emacs set up
-if which cask >/dev/null 2>&1; then
-  echo "setup .emacs.d..."
-  cd ${THIS_DIR}/.emacs.d
-  cask upgrade
-  cask install
-fi
-
-cat << END
-
-**************************************************
-DOTFILES SETUP FINISHED! bye.
-**************************************************
-
-END
